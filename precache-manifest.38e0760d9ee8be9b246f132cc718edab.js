cd32b182653628e2eecb7dd823c4bf9e36bd3448
George Thomas Shanti <georgeshanti@gmail.com>
2020-03-24 00:02:20 +0530

    first commit

---
+++ precache-manifest.38e0760d9ee8be9b246f132cc718edab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583652e1c6956d0de37d629decca053c",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "62c2b609e04ab488b2e4",
+    "url": "/blog/static/css/main.5fcdb8ed.chunk.css"
+  },
+  {
+    "revision": "cc2fd9ac4924e4cd14bb",
+    "url": "/blog/static/js/2.ea11982d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blog/static/js/2.ea11982d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62c2b609e04ab488b2e4",
+    "url": "/blog/static/js/main.9d878bbc.chunk.js"
+  },
+  {
+    "revision": "e71afbe5f7dcee561e99",
+    "url": "/blog/static/js/runtime-main.c1962588.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blog/static/media/logo.5d5d9eef.svg"
+  }
+]);
